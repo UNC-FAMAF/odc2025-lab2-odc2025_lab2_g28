1568da54f8212623575c826e98d043235d4db1c9
aguatines <agustina.sosa.559@mi.unc.edu.ar>
2025-05-21 19:08:30 -0300

    Cambio del color de fondo, función para hacer circulos y para pintar un pixel

--- ejercicio1/app.s
+++ ejercicio1/app.s
@@ -1,5 +1,5 @@
 	.equ SCREEN_WIDTH, 		640
-	.equ SCREEN_HEIGH, 		480
+	.equ SCREEN_HEIGHT, 		480
 	.equ BITS_PER_PIXEL,  	32
 
 	.equ GPIO_BASE,      0x3f200000
@@ -7,47 +7,48 @@
 	.equ GPIO_GPLEV0,    0x34
 
 	.globl main
+	.extern dibujar_circulo
+	.extern dibujar_pixel
 
 main:
 	// x0 contiene la direccion base del framebuffer
- 	mov x20, x0	// Guarda la dirección base del framebuffer en x20
+ 	mov x20, x0       // Backup del framebuffer en x20 
+	mov x21, x0
 	//---------------- CODE HERE ------------------------------------
 
-	movz x10, 0xC7, lsl 16
-	movk x10, 0x1585, lsl 00
+	movz x10, 0x5B6E, lsl 16 // Color celeste para el cielo
+	movk x10, 0xE1, lsl 0 
 
-	mov x2, SCREEN_HEIGH         // Y Size
+	mov x2, SCREEN_HEIGHT         // Y Size
 loop1:
 	mov x1, SCREEN_WIDTH         // X Size
 loop0:
-	stur w10,[x0]  // Colorear el pixel N
-	add x0,x0,4	   // Siguiente pixel
+	stur w10,[x20]  // Colorear el pixel N
+	add x20,x20,4	   // Siguiente pixel
 	sub x1,x1,1	   // Decrementar contador X
 	cbnz x1,loop0  // Si no terminó la fila, salto
 	sub x2,x2,1	   // Decrementar contador Y
 	cbnz x2,loop1  // Si no es la última fila, salto
 
-	// Ejemplo de uso de gpios
-	mov x9, GPIO_BASE
+	mov x0, x21 // framebuffer base
+	mov x1, 326
+	mov x2, 285
+	movz x4, 0xFFFF, lsl 16
+	movk x4, 0xFFFF, lsl 0
+	bl dibujar_pixel
 
-	// Atención: se utilizan registros w porque la documentación de broadcom
-	// indica que los registros que estamos leyendo y escribiendo son de 32 bits
+    mov x0, x21          // framebuffer base
+    mov x1, 320          // centro x
+    mov x2, 240          // centro y
+    mov x3, 100          // radio
 
-	// Setea gpios 0 - 9 como lectura
-	str wzr, [x9, GPIO_GPFSEL0]
+    movz w4, 0xFFFF
+    movk w4, 0xFFFF, lsl 16   // color blanco 0xFFFFFFFF (32 bits)
 
-	// Lee el estado de los GPIO 0 - 31
-	ldr w10, [x9, GPIO_GPLEV0]
+    bl dibujar_circulo
 
-	// And bit a bit mantiene el resultado del bit 2 en w10
-	and w11, w10, 0b10
 
-	// w11 será 1 si había un 1 en la posición 2 de w10, si no será 0
-	// efectivamente, su valor representará si GPIO 2 está activo
-	lsr w11, w11, 1
 
-	//---------------------------------------------------------------
-	// Infinite Loop
 
 InfLoop:
 	b InfLoop
